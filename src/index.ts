1d3d9f6cf8773dd097f6f15ad56e863891d562b9
Сандро <warsan00@mail.ru>
2020-10-01 05:43:12 +0500

    Правки

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+document.getElementById("app").innerHTML = 
+`
+<h1>Parsel</h1>
+<h3>Привет, пакет!</h3>
+<div>
+<p>Молниеносно быстрый упаковщик для веб-приложений без настроек</p>
+  <a href="https://ru.parceljs.org/" target="_blank" rel="noopener noreferrer">Смотри здесь</a>
+  для получения дополнительной информации о пакете. Документация уже на русском языке.
+</div>
+`;
